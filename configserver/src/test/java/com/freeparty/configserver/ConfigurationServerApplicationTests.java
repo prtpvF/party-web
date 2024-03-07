8533fe95ed896b09572b6bbb2a8539501beb80cb
Mihail Chaplygin <caplyginmihail48@gmail.com>
2024-03-07 23:33:40 +0300

    add spring config server

---
+++ configserver/src/test/java/com/freeparty/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeparty.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
