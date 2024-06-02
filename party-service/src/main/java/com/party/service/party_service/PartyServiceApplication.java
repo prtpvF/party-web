dcd4723b53654da00249d58017ff9381b299f80b
Mihail Chaplygin <caplyginmihail48@gmail.com>
2024-06-02 21:10:16 +0300

    add party-service

---
+++ party-service/src/main/java/com/party/service/party_service/PartyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.party.service.party_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyServiceApplication.class, args);
+	}
+
+}
---
+++ party-service/src/test/java/com/party/service/party_service/PartyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.party.service.party_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
